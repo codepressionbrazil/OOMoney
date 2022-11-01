6cf8154caeb067d1cccc8f245b007a0f7e37387b
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-11-01 10:26:07 -0300

    Initial commit

---
+++ src/main/java/br/codepressionbrazil/OOMoney/OoMoneyApplication.java
@@ -0,0 +1,13 @@
+package br.codepressionbrazil.OOMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/codepressionbrazil/OOMoney/OoMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.codepressionbrazil.OOMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
